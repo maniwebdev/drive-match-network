ca4bf865633d5e7428400d637db789e12cf819d4
Muhammad Usman <manijnb0333@gmail.com>
2024-12-19 16:59:43 +0500

    done

--- components/Authentication/ProfileModal/UpdateProfileModal.js
+++ components/Authentication/ProfileModal/UpdateProfileModal.js
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable react/no-unescaped-entities */
 import React, { useState, useEffect } from 'react';
 import styles from '../../../styles/Profile/updateModal.module.css';
 import { Modal, Form, Input, Switch, message } from 'antd';
